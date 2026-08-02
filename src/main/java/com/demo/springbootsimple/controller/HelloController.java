e4d5f828e3b6e0c5c9d1c24bc0e4baf85e2b309f
YLong <YLong@233.com>
2026-08-02 18:22:02 +0800

    springboot，三种传参方式，entity实体类，映射数据库表

--- src/main/java/com/demo/springbootsimple/controller/HelloController.java
+++ src/main/java/com/demo/springbootsimple/controller/HelloController.java
@@ -1,24 +1,45 @@
 package com.demo.springbootsimple.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.demo.springbootsimple.entity.User;
+import org.springframework.web.bind.annotation.*;
+
+import javax.sound.midi.Soundbank;
 import java.util.HashMap;
 import java.util.Map;
 
 @RestController
 public class HelloController {
+    /*@GetMapping("/user2")
+   public Map<String, Object> getUser() {
+       Map<String, Object> map = new HashMap<>();
+       map.put("name", "cyl");
+       map.put("age", 22);
+       return map;
+   }*/
 
-    @GetMapping("/hello")
-    public String hello() {
+    //———————————————————————————三种传参方式—————————————————————
+   /* @GetMapping("/hello")
+    public String hello(@RequestParam(defaultValue = "") String name) {
+        System.out.println(name);
+        return "hello springboot!";
+    }*/
+
+    /*@GetMapping("/hell/{name}")//路径映射要对！
+    public String hell(@PathVariable String name) {
+        System.out.println(name);
+        return "Hello SpringBoot!";
+    }*/
+
+    @PostMapping("/user")//get只能get，这里要用post！
+    public String hell(@RequestBody User user) {
+        System.out.println("name:"+user.getName());
+        System.out.println("age:"+user.getAge());
         return "Hello SpringBoot!";
     }
-    @GetMapping("/user")
-    public Map<String,Object> getUser(){
-        Map<String,Object> map = new HashMap<>();
-        map.put("name","cyl");
-        map.put("age",22);
-        return map;
-    }
+
+
+
+
 
 }
 
